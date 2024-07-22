795a92c455513957bf86c9def657df48a757f965
MetagesHailu1 <133263306+MetagesHailu1@users.noreply.github.com>
2024-07-22 08:34:34 -0700

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let scrollHeight =
  document.documentElement.scrollHeight - document.documentElement.clientHeight;   //document.documentElement.something (new concept) 
 
 window.addEventListener("scroll" , scroll);    //window.addEventListener("scroll" , scroll); (new concept)
+
 function scroll(){
 
     let scrollTop= document.documentElement.scrollTop;
